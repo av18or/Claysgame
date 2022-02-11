d1ebee58f12c70c89570403f36128515ca72a7ac
Clayton <claytongorham@gmail.com>
2022-02-10 20:59:53 -0800

    add event listeners

--- Readme.md
+++ Readme.md
@@ -1,1 +1,11 @@
 This is Clay's browser game# Claysgame
+
+This is a basic tic tac toe game. 
+It can be played single player or two players if you're both sitting at the same computer. 
+
+Player 1 is X.
+Player 2 is O.
+
+Three in a row wins the game. 
+
+
---
+++ script.js
@@ -0,0 +1,53 @@
+document.querySelectorAll('#one')
+.forEach(e => e.addEventListener("click", function() {
+    // Here, `this` refers to the element the event was hooked on
+    console.log("1clicked")
+}));
+
+document.querySelectorAll('#two')
+.forEach(e => e.addEventListener("click", function() {
+    // Here, `this` refers to the element the event was hooked on
+    console.log("2clicked")
+}));
+
+document.querySelectorAll('#three')
+.forEach(e => e.addEventListener("click", function() {
+    // Here, `this` refers to the element the event was hooked on
+    console.log("3clicked")
+}));
+
+document.querySelectorAll('#four')
+.forEach(e => e.addEventListener("click", function() {
+    // Here, `this` refers to the element the event was hooked on
+    console.log("4clicked")
+}));
+
+document.querySelectorAll('#five')
+.forEach(e => e.addEventListener("click", function() {
+    // Here, `this` refers to the element the event was hooked on
+    console.log("5clicked")
+}));
+
+document.querySelectorAll('#six')
+.forEach(e => e.addEventListener("click", function() {
+    // Here, `this` refers to the element the event was hooked on
+    console.log("6clicked")
+}));
+
+document.querySelectorAll('#seven')
+.forEach(e => e.addEventListener("click", function() {
+    // Here, `this` refers to the element the event was hooked on
+    console.log("7clicked")
+}));
+
+document.querySelectorAll('#eight')
+.forEach(e => e.addEventListener("click", function() {
+    // Here, `this` refers to the element the event was hooked on
+    console.log("8clicked")
+}));
+
+document.querySelectorAll('#nine')
+.forEach(e => e.addEventListener("click", function() {
+    // Here, `this` refers to the element the event was hooked on
+    console.log("9clicked")
+}));
