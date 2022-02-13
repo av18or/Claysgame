9901f4a450741b246c7eba892b3bfa05b6e922d1
Clayton <claytongorham@gmail.com>
2022-02-13 11:46:30 -0800

    add some variables to script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+//define variables and create array containing arrays. 
+
+const playerX = 'x'
+const playerO = 'circle'
+const combinations = [
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 6]
+]
+
+
+// define variables for each cell on the board, and button. 
+
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+let circleTurn
