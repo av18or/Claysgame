d16bbbbf85ed70a57277babd88fb28c03f1e2845
Clayton <claytongorham@gmail.com>
2022-02-13 11:54:58 -0800

    begin adding functions

--- script.js
+++ script.js
@@ -22,3 +22,40 @@ const winningMessageElement = document.getElementById('winningMessage')
 const restartButton = document.getElementById('restartButton')
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 let circleTurn
+
+startGame()  //call startgame
+
+restartButton.addEventListener('click', startGame)
+
+
+// add fucntions 
+
+function startGame() {
+    circleTurn = false
+    cellElements.forEach(cell => {
+        cell.classList.remove(playerX)
+        cell.classList.remove(playerO)
+        cell.removeEventListener('click', handleClick)
+        cell.addEventListener('click', handleClick, {once: true})
+    })
+
+    setBoardHoverClass()
+    winningMessageElement.classList.remove('show')
+}
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? playerO : playerX
+    placeMark(cell, currentClass)
+    if (checkWin(currentClass)) {
+      endGame(false)
+    } else if (isDraw()) {
+      endGame(true)
+    } else {
+      swapTurns()
+      setBoardHoverClass()
+    }
+  }
+
+
+  // create remaining functions
